0092c69956894e0d449099ccf773fccbfe5ea2d2
Evan Chiang <happyday9837@gmail.com>
2022-12-25 03:43:39 +0800

    added:專案開啟

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+  <section class="list-box ">
+    <ul class="">
+      <li></li>
+    </ul>
+  </section>
+</template>
+<style lang='scss' scoped>
+.list-box{
+  width: 250px;
+  height: 100vh;
+  background-color: #A1FFC7;
+  // margin-top:12px ;
+  // margin-left: 21px;
+  // margin-right: 20px;
+  // margin-bottom: 9px;
+}
+</style>
---
+++ src/views/TodolistView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ListComponent from '@/components/ListComponent.vue';
+
+</script>
+<template>
+<ListComponent/>
+</template>
+<style lang='scss' scoped>
+</style>
