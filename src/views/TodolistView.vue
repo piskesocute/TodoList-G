42cfe5941c21c6c11a007326dff32d66eedac7dc
Evan Chiang <happyday9837@gmail.com>
2022-12-26 06:55:48 +0800

    updated:新增 手機版開啟時的遮罩

--- src/views/TodolistView.vue
+++ src/views/TodolistView.vue
@@ -5,15 +5,14 @@ import { useTodoData } from '@/stores/todoData';
 import { storeToRefs } from 'pinia';
 
 const store = useTodoData();
+
 const { navOpen } = storeToRefs(store);
 
 </script>
 <template>
-  <div class=" d-flex justify-content-end align-items-center">
-  </div>
   <section class="d-flex justify-content-center align-items-center position-relative">
     <ListComponent class="list-component" :class="{actived:navOpen}"></ListComponent>
-    <WhatTodoComponent class="whattodo-component"></WhatTodoComponent>
+    <WhatTodoComponent class="whattodo-component" :class="{actived:navOpen}"></WhatTodoComponent>
   </section>
 
 </template>
@@ -27,17 +26,43 @@ const { navOpen } = storeToRefs(store);
     position: absolute;
     top: 0;
     left: 0;
-    z-index: 1;
+    z-index: 2;
     transform:translate(-100%) ;
     transition: .2s;
     &.actived{
       transform:translate(0%);
       transition: .2s;
+
     }
   }
 }
 .whattodo-component{
   width: 100%;
   height: 100vh;
+  position: relative;
+  transition: .5s;
+  &:before{
+    display: block;
+    position: absolute;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    content: '';
+    transform: translate(-100%,0);
+    background-color: rgba(0, 0, 0, .6);
+    background-size: cover;
+    background-position: center center;
+    transition: .2s ease-in;
+    opacity: 0;
+  }
+  &.actived:before{
+    transform: translate(0,0);
+    transition: .2s;
+    opacity: 1;
+
+  }
 }
+
 </style>
