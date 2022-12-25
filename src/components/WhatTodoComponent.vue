3e3e86ca84a165e37b226bd3693391ae67350e23
Evan Chiang <happyday9837@gmail.com>
2022-12-26 06:55:09 +0800

    updated:綁定清單資料

--- src/components/WhatTodoComponent.vue
+++ src/components/WhatTodoComponent.vue
@@ -1,16 +1,26 @@
 <script setup>
 import { useTodoData } from '@/stores/todoData';
+import { computed, onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
 
 const store = useTodoData();
-const { changeNav } = store;
+const { changeNav, removeItem, changeList } = store;
+const { nowList } = storeToRefs(store);
+const textLength = computed(() => nowList.value.content.length);
+
+onMounted(() => {
+  changeList();
+});
 </script>
 <template>
   <section class="whattodo-box">
-    <div class="d-flex justify-content-between align-items-center">
-      <button type="button" class="btn mb-24 btn-sm" @click="changeNav">
+    <div class="d-flex justify-content-between justify-content-md-end align-items-center">
+      <button type="button" class="btn mb-24 btn-sm d-block d-md-none" @click="changeNav">
         <i class="bi bi-list fs-20"></i>
       </button>
-      <button type="button" class="btn mb-24 btn-sm">
+      <button type="button" class="btn mb-24 btn-sm"
+        @click="removeItem"
+      >
         <i class="bi bi-trash-fill fs-20"></i>
       </button>
     </div>
@@ -22,19 +32,21 @@ const { changeNav } = store;
             class="form-control form-control-lg"
             type="text"
             placeholder="new item title"
+            v-model="nowList.title"
           />
         </div>
       </div>
-      <div class="row gx-lg-24">
-        <div class="col-md-7">
+      <div class="row gx-0 gx-lg-24 gy-24 gy-lg-0">
+        <div class="col-12 col-lg-7">
           <div class="position-relative mb-24">
             <textarea
               class="form-control pb-64"
               placeholder="content"
               style="height: 150px"
               maxlength="200"
+              v-model="nowList.content"
             ></textarea>
-            <span class="textlenght">1/200</span>
+            <span class="textlenght">{{textLength}}/200</span>
           </div>
 
           <div class="d-flex justify-content-between align-items-center">
@@ -43,7 +55,7 @@ const { changeNav } = store;
             <input type="date" class="form-control text-center fs-20" />
           </div>
         </div>
-        <div class="col-md-5">
+        <div class="col-12 col-lg-5">
           <div class="img-box">
             <img class="todo-img rounded-2 mb-24" src="/sausage.png" alt="" />
             <label class="w-100 btn btn-lg btn-btn-green">
@@ -64,8 +76,8 @@ const { changeNav } = store;
   position: absolute;
   font-size: 14px;
   line-height: 15px;
-  bottom: 17px;
-  right: 17px;
+  bottom: 10px;
+  right: 30px;
 }
 .img-box {
   width: 100%;
