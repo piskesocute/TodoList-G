4306dc9a9b90f9a1cb6615bc5442536bfe006d74
Evan Chiang <happyday9837@gmail.com>
2022-12-25 18:45:01 +0800

    added: ListComponent & WhatTodoComponent

--- src/views/TodolistView.vue
+++ src/views/TodolistView.vue
@@ -1,9 +1,22 @@
 <script setup>
 import ListComponent from '@/components/ListComponent.vue';
-
+import WhatTodoComponent from '@/components/WhatTodoComponent.vue';
 </script>
 <template>
-<ListComponent/>
+  <section class="d-flex justify-content-center align-items-center">
+    <ListComponent class="list-component"></ListComponent>
+    <WhatTodoComponent class="whattodo-component"></WhatTodoComponent>
+  </section>
+
 </template>
 <style lang='scss' scoped>
+.list-component{
+  width: 250px;
+  height: 100vh;
+  background-color: #a1ffc7;
+}
+.whattodo-component{
+  width: 100%;
+  height: 100vh;
+}
 </style>
