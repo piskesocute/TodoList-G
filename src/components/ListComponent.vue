1d4650c65ac86684cac0a8f19d2a01d9b7cf212f
Evan Chiang <happyday9837@gmail.com>
2022-12-25 18:44:20 +0800

    added:list-box layout

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -1,19 +1,68 @@
 <script setup></script>
 <template>
-  <section class="list-box ">
-    <ul class="">
-      <li></li>
+  <section class="list-box">
+    <ul class="list-unstyled d-flex aliic flex-column">
+      <h1 class="fs-20 fw-bold">Demo Todo List</h1>
+      <li
+        v-for="(item, index) in 10"
+        :key="index + 103021"
+        class="list-item bg-hover-green fs-20 lh-base"
+      >
+        <p class="m-0 p-1">Item title</p>
+        <div class="list-triangle active"></div>
+      </li>
     </ul>
+    <div class="d-flex justify-content-center align-items-center">
+      <button type="button" class="add-btn">Add item</button>
+
+    </div>
   </section>
 </template>
 <style lang='scss' scoped>
-.list-box{
-  width: 250px;
-  height: 100vh;
-  background-color: #A1FFC7;
-  // margin-top:12px ;
-  // margin-left: 21px;
-  // margin-right: 20px;
-  // margin-bottom: 9px;
+.list-box {
+  width: 100%;
+  height: 100%;
+  h1 {
+    margin-top: 12px;
+    margin-left: 21px;
+    margin-right: 20px;
+    margin-bottom: 9px;
+  }
+}
+.list-item {
+  margin-bottom: 8px;
+  padding-top: 4px;
+  padding-left: 13px;
+  padding-right: 31px;
+  padding-bottom: 3px;
+  font-size: 20px;
+  font-weight: 400;
+  position: relative;
+  overflow: hidden;
+  .active:after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 27px 31px 27px 0px;
+    border-color: transparent rgb(255, 255, 255) transparent transparent;
+    position: absolute;
+    right: -8px;
+    top: 50%;
+    transform: translate(0%, -50%);
+  }
+}
+.add-btn{
+  border: none;
+  border-radius: 10px;
+  background-color: #E7FFE9;
+  padding: 9px 41px 9px 36px;
+  font-size: 20px;
+  font-weight: 400;
+  transition: .2s;
+  &:hover{
+    background:#81F8B1 ;
+    transition: .2s;
+  }
 }
 </style>
