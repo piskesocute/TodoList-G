0bccfe0942b5820707065d20ba761797f3b1d093
Evan Chiang <happyday9837@gmail.com>
2022-12-26 09:48:23 +0800

    added:新增本地端圖片預覽、行事曆功能

--- src/components/WhatTodoComponent.vue
+++ src/components/WhatTodoComponent.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Datepicker from 'vue3-datepicker';
 import { useTodoData } from '@/stores/todoData';
 import { computed, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
@@ -8,19 +9,36 @@ const { changeNav, removeItem, changeList } = store;
 const { nowList } = storeToRefs(store);
 const textLength = computed(() => nowList.value.content.length);
 
+const imgUpload = (e) => {
+  const fileData = e.target.files[0];
+  const reader = new FileReader();
+  reader.readAsDataURL(fileData);
+  reader.addEventListener('load', (event) => {
+    nowList.value.img = event.target.result;
+  });
+};
+
 onMounted(() => {
   changeList();
 });
 </script>
 <template>
   <section class="whattodo-box">
-    <div class="d-flex justify-content-between justify-content-md-end align-items-center">
-      <button type="button" class="btn mb-24 btn-sm d-block d-md-none" @click="changeNav">
+    <div
+      class="
+        d-flex
+        justify-content-between justify-content-md-end
+        align-items-center
+      "
+    >
+      <button
+        type="button"
+        class="btn mb-24 btn-sm d-block d-md-none"
+        @click="changeNav"
+      >
         <i class="bi bi-list fs-20"></i>
       </button>
-      <button type="button" class="btn mb-24 btn-sm"
-        @click="removeItem"
-      >
+      <button type="button" class="btn mb-24 btn-sm" @click="removeItem">
         <i class="bi bi-trash-fill fs-20"></i>
       </button>
     </div>
@@ -46,23 +64,38 @@ onMounted(() => {
               maxlength="200"
               v-model="nowList.content"
             ></textarea>
-            <span class="textlenght">{{textLength}}/200</span>
+            <span class="textlenght">{{ textLength }}/200</span>
           </div>
 
           <div class="d-flex justify-content-between align-items-center">
-            <input type="date" class="form-control text-center fs-20" />
+            <Datepicker
+              class="form-control text-center fs-20"
+              v-model="nowList.dateFrom"
+            />
             <span class="px-32"> ~ </span>
-            <input type="date" class="form-control text-center fs-20" />
+            <Datepicker
+              class="form-control text-center fs-20"
+              v-model="nowList.dateTo"
+              :lower-limit="nowList.dateFrom"
+            />
           </div>
         </div>
         <div class="col-12 col-lg-5">
-          <div class="img-box">
-            <img class="todo-img rounded-2 mb-24" src="/sausage.png" alt="" />
-            <label class="w-100 btn btn-lg btn-btn-green">
-                Upload Image
-              <input type="file" style="display: none"/>
-            </label>
+          <div
+            class="img-box rounded-2 mb-24"
+            :class="{ noimg: nowList.img === '' }"
+          >
+            <img
+              v-if="nowList.img !== ''"
+              class="todo-img rounded-2"
+              :src="nowList.img"
+              alt=""
+            />
           </div>
+          <label class="w-100 btn btn-lg btn-btn-green">
+            Upload Image
+            <input type="file" style="display: none" @change="imgUpload" />
+          </label>
         </div>
       </div>
     </div>
@@ -71,6 +104,7 @@ onMounted(() => {
 <style lang='scss' scoped>
 .whattodo-box {
   padding: 23px 17px 23px 25px;
+  width: 100%;
 }
 .textlenght {
   position: absolute;
@@ -82,6 +116,15 @@ onMounted(() => {
 .img-box {
   width: 100%;
   height: 150px;
+  background-color: #e1e1e1;
+  position: relative;
+  &.noimg::before {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    content: '請上傳圖片';
+  }
 }
 .todo-img {
   width: 100%;
