40251b25ccb9aa197dc6d1842d72e095f703cc21
Evan Chiang <happyday9837@gmail.com>
2022-12-26 09:49:48 +0800

    fixed:修復寬度無法固定問題

--- src/views/TodolistView.vue
+++ src/views/TodolistView.vue
@@ -7,41 +7,49 @@ import { storeToRefs } from 'pinia';
 const store = useTodoData();
 
 const { navOpen } = storeToRefs(store);
-
 </script>
 <template>
-  <section class="d-flex justify-content-center align-items-center position-relative">
-    <ListComponent class="list-component" :class="{actived:navOpen}"></ListComponent>
-    <WhatTodoComponent class="whattodo-component" :class="{actived:navOpen}"></WhatTodoComponent>
+  <section
+    class="d-flex justify-content-center align-items-center position-relative"
+  >
+    <ListComponent
+      class="list-component"
+      :class="{ actived: navOpen }"
+    ></ListComponent>
+    <WhatTodoComponent
+      class="whattodo-component"
+      :class="{ actived: navOpen }"
+    ></WhatTodoComponent>
   </section>
-
 </template>
 <style lang='scss' scoped>
-.list-component{
+.list-component {
   width: 250px;
   height: 100vh;
   background-color: #a1ffc7;
-  @include md{
+  @include md {
     width: 320px;
     position: absolute;
     top: 0;
     left: 0;
     z-index: 2;
-    transform:translate(-100%) ;
-    transition: .2s;
-    &.actived{
-      transform:translate(0%);
-      transition: .2s;
-
+    transform: translate(-100%);
+    transition: 0.2s;
+    &.actived {
+      transform: translate(0%);
+      transition: 0.2s;
     }
   }
 }
-.whattodo-component{
-  width: 100%;
+.whattodo-component {
+  width: calc(100vw - 250px);
   height: 100vh;
   position: relative;
-  transition: .5s;
-  &:before{
+  transition: 0.5s;
+
+  @include md{
+    width: 100%;
+    &:before{
     display: block;
     position: absolute;
     z-index: 1;
@@ -50,19 +58,18 @@ const { navOpen } = storeToRefs(store);
     width: 100%;
     height: 100vh;
     content: '';
-    transform: translate(-100%,0);
+    transform: translate(-200%,0);
     background-color: rgba(0, 0, 0, .6);
     background-size: cover;
     background-position: center center;
-    transition: .2s ease-in;
-    opacity: 0;
+    transition: .1s ease-in;
   }
   &.actived:before{
     transform: translate(0,0);
     transition: .2s;
-    opacity: 1;
 
   }
-}
+  }
 
+}
 </style>
